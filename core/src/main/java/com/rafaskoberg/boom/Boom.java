9e7a3619ea89955e010a09559a560687c020af56
rafaskb <rafa.skoberg@hotmail.com>
2020-04-12 11:38:52 -0300

    Add documentation to Boom and BoomChannel classes.

--- core/src/main/java/com/rafaskoberg/boom/Boom.java
+++ core/src/main/java/com/rafaskoberg/boom/Boom.java
@@ -7,12 +7,27 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
 
+/**
+ * Play music tracks and sound effects with the capability of having special post-processing effects, such as reverb and
+ * filters.
+ */
 public abstract class Boom {
 
+    /**
+     * Initiates Boom using an existing backend. Currently only lwjgl3 is supported.
+     *
+     * @return A Boom instance that can be used to play sounds and music.
+     */
     public static Boom init() {
         return init(false);
     }
 
+    /**
+     * Initiates Boom using an existing backend. Currently only lwjgl3 is supported.
+     *
+     * @param verbose Whether or not the init process should be logged to the console.
+     * @return A Boom instance that can be used to play sounds and music.
+     */
     public static Boom init(boolean verbose) {
         // Get application
         if(Gdx.app == null) {
@@ -53,63 +68,203 @@ public abstract class Boom {
         return boom;
     }
 
+    /**
+     * Creates a new {@link BoomChannel} using the given ID.
+     *
+     * @throws IllegalStateException If there's already a channel registered to the given ID.
+     */
     public abstract BoomChannel createChannel(int channelId);
 
+    /**
+     * Returns the {@link BoomChannel} associated with the given ID, if any.
+     */
     public abstract BoomChannel getChannel(int id);
 
+    /**
+     * Plays the sound. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound     Sound to be played.
+     * @param channelId The ID of the channel this sound should be played through.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long play(Sound sound, int channelId) {
         return play(sound, getChannel(channelId), 1, 1, 0);
     }
 
+    /**
+     * Plays the sound. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound     Sound to be played.
+     * @param channelId The ID of the channel this sound should be played through.
+     * @param volume    the volume in the range [0,1]
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long play(Sound sound, int channelId, float volume) {
         return play(sound, getChannel(channelId), volume, 1, 0);
     }
 
+    /**
+     * Plays the sound. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound     Sound to be played.
+     * @param channelId The ID of the channel this sound should be played through.
+     * @param volume    the volume in the range [0,1]
+     * @param pitch     the pitch multiplier, 1 == default, >1 == faster, <1 == slower, the value has to be between 0.5
+     *                  and 2.0
+     * @param pan       panning in the range -1 (full left) to 1 (full right). 0 is center position.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long play(Sound sound, int channelId, float volume, float pitch, float pan) {
         return play(sound, getChannel(channelId), volume, pitch, pan);
     }
 
+    /**
+     * Plays the sound. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound   Sound to be played.
+     * @param channel Channel this sound should be played through.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long play(Sound sound, BoomChannel channel) {
         return play(sound, channel, 1, 1, 0);
     }
 
+    /**
+     * Plays the sound. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound   Sound to be played.
+     * @param channel Channel this sound should be played through.
+     * @param volume  the volume in the range [0,1]
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long play(Sound sound, BoomChannel channel, float volume) {
         return play(sound, channel, volume, 1, 0);
     }
 
+    /**
+     * Plays the sound. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound   Sound to be played.
+     * @param channel Channel this sound should be played through.
+     * @param volume  the volume in the range [0,1]
+     * @param pitch   the pitch multiplier, 1 == default, >1 == faster, <1 == slower, the value has to be between 0.5
+     *                and 2.0
+     * @param pan     panning in the range -1 (full left) to 1 (full right). 0 is center position.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public abstract long play(Sound sound, BoomChannel channel, float volume, float pitch, float pan);
 
+    /**
+     * Plays the sound, looping it forever. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound     Sound to be played.
+     * @param channelId The ID of the channel this sound should be played through.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long loop(Sound sound, int channelId) {
         return loop(sound, getChannel(channelId), 1, 1, 0);
     }
 
+    /**
+     * Plays the sound, looping it forever. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound     Sound to be played.
+     * @param channelId The ID of the channel this sound should be played through.
+     * @param volume    the volume in the range [0, 1]
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long loop(Sound sound, int channelId, float volume) {
         return loop(sound, getChannel(channelId), volume, 1, 0);
     }
 
+    /**
+     * Plays the sound, looping it forever. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound     Sound to be played.
+     * @param channelId The ID of the channel this sound should be played through.
+     * @param volume    the volume in the range [0, 1]
+     * @param pitch     the pitch multiplier, 1 == default, >1 == faster, <1 == slower, the value has to be between 0.5
+     *                  and 2.0
+     * @param pan       panning in the range -1 (full left) to 1 (full right). 0 is center position.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long loop(Sound sound, int channelId, float volume, float pitch, float pan) {
         return loop(sound, getChannel(channelId), volume, pitch, pan);
     }
 
+    /**
+     * Plays the sound, looping it forever. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound   Sound to be played.
+     * @param channel Channel this sound should be played through.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long loop(Sound sound, BoomChannel channel) {
         return loop(sound, channel, 1, 1, 0);
     }
 
+    /**
+     * Plays the sound, looping it forever. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound   Sound to be played.
+     * @param channel Channel this sound should be played through.
+     * @param volume  the volume in the range [0, 1]
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public final long loop(Sound sound, BoomChannel channel, float volume) {
         return loop(sound, channel, volume, 1, 0);
     }
 
+    /**
+     * Plays the sound, looping it forever. If the sound is already playing, it will be played again, concurrently.
+     *
+     * @param sound   Sound to be played.
+     * @param channel Channel this sound should be played through.
+     * @param volume  the volume in the range [0, 1]
+     * @param pitch   the pitch multiplier, 1 == default, >1 == faster, <1 == slower, the value has to be between 0.5
+     *                and 2.0
+     * @param pan     panning in the range -1 (full left) to 1 (full right). 0 is center position.
+     * @return the id of the sound instance if successful, or -1 on failure.
+     */
     public abstract long loop(Sound sound, BoomChannel channel, float volume, float pitch, float pan);
 
+    /**
+     * Starts the play back of the music stream. In case the stream was paused this will resume the play back. In case
+     * the music stream is finished playing this will restart the play back.
+     *
+     * @param music     Music to be played.
+     * @param channelId The ID of the channel this music should be played through.
+     */
     public final void play(Music music, int channelId) {
         play(music, getChannel(channelId));
     }
 
+    /**
+     * Starts the play back of the music stream. In case the stream was paused this will resume the play back. In case
+     * the music stream is finished playing this will restart the play back.
+     *
+     * @param music   Music to be played.
+     * @param channel Channel this music should be played through.
+     */
     public abstract void play(Music music, BoomChannel channel);
 
+    /**
+     * Changes the {@link BoomChannel} the given music is playing through. The music must be playing for this method to
+     * have any effect.
+     *
+     * @param music     Music to have its channel changed.
+     * @param channelId The ID of the channel this music should be played through.
+     */
     public final void changeMusicChannel(Music music, int channelId) {
         changeMusicChannel(music, getChannel(channelId));
     }
 
+    /**
+     * Changes the {@link BoomChannel} the given music is playing through. The music must be playing for this method to
+     * have any effect.
+     *
+     * @param music   Music to have its channel changed.
+     * @param channel Channel this music should be played through.
+     */
     public abstract void changeMusicChannel(Music music, BoomChannel channel);
 }
--- core/src/main/java/com/rafaskoberg/boom/BoomChannel.java
+++ core/src/main/java/com/rafaskoberg/boom/BoomChannel.java
@@ -9,6 +9,9 @@ import com.rafaskoberg.boom.effect.echo.EchoPreset;
 import com.rafaskoberg.boom.effect.reverb.ReverbData;
 import com.rafaskoberg.boom.effect.reverb.ReverbPreset;
 
+/**
+ * Channel where sound effects and music tracks can be played through.
+ */
 public abstract class BoomChannel {
     private final int   id;
     private       float sourceGain   = 1.0f;
@@ -25,38 +28,79 @@ public abstract class BoomChannel {
         return id;
     }
 
+    /**
+     * Returns the volume gain of the source audio, before any effects are applied.
+     */
     public float getSourceGain() {
         return sourceGain;
     }
 
+    /**
+     * Sets volume gain of the source audio, before any effects are applied.
+     */
     public void setSourceGain(float sourceGain) {
         this.sourceGain = MathUtils.clamp(sourceGain, 0, 1);
     }
 
+    /**
+     * Returns the source audio low pass filter cutoff, from 0 to 1. The lower this value is, less high frequencies will
+     * be played.
+     */
     public float getSourceCutoff() {
         return sourceCutoff;
     }
 
+    /**
+     * Sets the source audio low pass filter cutoff, from 0 to 1. The lower this value is, less high frequencies will be
+     * played.
+     */
     public void setSourceCutoff(float sourceCutoff) {
         this.sourceCutoff = MathUtils.clamp(sourceCutoff, 0, 1);
     }
 
+    /**
+     * Adds a reverb effect to this channel.
+     */
     public abstract BoomEffect addReverb(ReverbPreset preset);
 
+    /**
+     * Adds a reverb effect to this channel.
+     */
     public abstract BoomEffect addReverb(ReverbData data);
 
+    /**
+     * Adds a distortion effect to this channel.
+     */
     public abstract BoomEffect addDistortion(DistortionPreset preset);
 
+    /**
+     * Adds a distortion effect to this channel.
+     */
     public abstract BoomEffect addDistortion(DistortionData data);
 
+    /**
+     * Adds an echo effect to this channel.
+     */
     public abstract BoomEffect addEcho(EchoPreset preset);
 
+    /**
+     * Adds an echo effect to this channel.
+     */
     public abstract BoomEffect addEcho(EchoData data);
 
+    /**
+     * Removes a certain effect from this channel.
+     */
     public abstract void removeEffect(BoomEffect effect);
 
+    /**
+     * Removes a certain effect from this channel.
+     */
     public abstract void removeEffect(int effectIndex);
 
+    /**
+     * Removes all effects from this channel.
+     */
     public abstract void removeAllEffects();
 
     protected abstract int getAlAuxSlot();
